26fb4576b7c1b4aaa69d204a1ed65df37bbe9816
Islom Toirjonov <toirjonovislomjon3@gmail.com>
2024-05-13 12:12:19 +0500

    done

---
+++ index.js
@@ -0,0 +1,67 @@
+// Examen JavaScript Amaliy savollar 
+
+// 1-savol Arifmetik operatorla
+// alert(5 % 2);
+
+
+// 2-savol  Math metodlari
+// let sonlar =Math.floor(Math.random()* 10);
+// alert(sonlar);
+
+
+// 3-savol Math metodlari
+// let son = Math.floor(12.510);
+// alert(son)
+
+
+// 4-savol function
+// let soz = "Mars It school"
+// function mars() {
+//     alert(soz.length);
+// }
+// mars();
+
+
+// 5-savol for loop
+// for (let i = 0; i < 100; i++) {
+//     console.log("Mars");
+// }
+
+
+// 6-savol Array methods
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+// let ism = harflar[0] + harflar[7] + harflar[23];
+// alert(ism);
+
+
+// 7 - vazifa
+// let yosh = prompt("Yoshingizni kiriting:");
+
+// if (yosh >= 18) {
+//     console.log("siz 18 yoshdan oshgansiz");
+// } else if (yosh < 18) {
+//     console.log("Siz 18 yoshdan kichiksiz");
+// } else {
+//     console.log("18 yoshda bo'lganingiz uchun voenkom sizni kutmoqda");
+// }
+
+// 8 - vazifa
+// let ism = prompt("Ismingizni kiriting:");
+// let teskariism = "";
+
+// for (var i = ism.length - 1; i >= 0; i--) {
+//     teskariism += ism[i];
+// }
+
+// console.log(teskariism);
+
+// 10 - vazifa
+// let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log("Juft sonlar:");
+
+// for (let i = 0; i < sonlar.length; i++) {
+//     if (sonlar[i] % 2 === 0) {
+//         console.log(sonlar[i]);
+//     }
+// }
